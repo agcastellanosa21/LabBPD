b6a9a7393d1788119cbae524111f728f0a990a10
acastellanos <andres.castellanos@keyrus.com>
2024-08-21 19:46:56 -0500

    first commit

---
+++ src/main/java/LabBPD/LabBpdApplication.java
@@ -0,0 +1,13 @@
+package LabBPD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabBpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabBpdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LabBPD/LabBpdApplicationTests.java
@@ -0,0 +1,13 @@
+package LabBPD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabBpdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
